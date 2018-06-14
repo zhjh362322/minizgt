bd181617ffea88956f1f5ba869b7cb8d83697103
baojian <252532242@qq.com>
2018-06-14 18:45:02 +0800

    接口初步完成

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,4 +1,3 @@
-var data = require('../../data/data.js');
 var app = getApp();
 Page({
 
@@ -10,16 +9,33 @@ Page({
   
   },
   formSubmit: function(e) {
-    app.globalData.userInfo = data.userInfo;
-    wx.setStorage({
-      key: 'userInfo',
-      data: data.userInfo,
+    var that = this;
+    var params = e.detail.value;
+    params.from = 'mini';
+    wx.request({
+      url: app.globalData.host + '/login',
+      data: params,
+      method: 'POST',
+      success: function(res) {
+        if(res.data.code == 0) {
+          that.setData({
+            err: res.data.msg
+          })
+        } else {
+          app.globalData.userInfo = res.data;
+          wx.setStorage({
+            key: 'userInfo',
+            data: res.data
+          })
+          that.setData({
+            hasUserInfo: true
+          })
+          wx.reLaunch({
+            url: '../personal/personal',
+          })
+        }
+      }
     })
-    this.setData({
-      hasUserInfo: true
-    })
-    wx.reLaunch({
-      url: '../personal/personal',
-    })
+
   }
 })
--- pages/personal/shipper/shipper.js
+++ pages/personal/shipper/shipper.js
@@ -1,15 +1,38 @@
 var data = require('../../../data/data.js');
+var app = getApp();
 Page({
   data: {
   
   },
 
   onLoad: function (options) {
-    var d = data.shipper;
-    var shipper = options.shipper;
-    if (d[shipper]) {
-      this.setData({
-        shipper: d[shipper]
+    var p = options.shipper
+    var userInfo = app.globalData.userInfo || wx.getStorageSync('userInfo');
+    if (userInfo && userInfo.level == 2) {
+      var consigner = [];
+      var consignee = [];
+      var shippers = userInfo.owner.plant.shipper;
+      console.log(shippers)
+      for(var i = 0; i <= shippers.length - 1; i++) {
+        var s = shippers[i];
+        if(s.type == 0) {
+          consigner.push(s);
+          consignee.push(s);
+        } else if (s.type == 1) {
+          consigner.push(s);
+        } else {
+          consignee.push(s);
+        }
+      }
+      if(p == 1) {
+        this.setData({ shipper: consigner });
+      } else {
+        this.setData({ shipper: consignee });
+      }
+    } else {
+      wx.showToast({
+        title: '请登录加盟商账号',
+        icon: 'none'
       })
     }
   }
